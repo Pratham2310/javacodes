c3367beadd7baa1d5f48833dc595f4537e577698
Prathamesh Soni <soni.prathmesh83@gmail.com>
2026-07-15 23:42:46 +0530

    just did some changes

--- lc88r.java
+++ lc88r.java
@@ -5,7 +5,7 @@ class solution{
         int j = n - 1;
         int k = m + n - 1;
 
-        while (j >= 0) {
+        while (j >= 0 && i>=0) {
             if (i >= 0 && nums1[i] > nums2[j]) {
                 nums1[k] = nums1[i];
                 i--;
@@ -14,6 +14,10 @@ class solution{
                 j--;
             }
             k--;
+        }
+        while(j>=0)
+        {
+            nums1[k--]=nums2[j--];
         }
 
         for (int l = 0; l < nums1.length; l++) {
