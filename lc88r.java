d7d97606b8fc084fcafb360ccd0f272b5644b53e
Prathamesh Soni <soni.prathmesh83@gmail.com>
2026-07-15 23:40:31 +0530

    solved leetcode 88 problem and got to know how to handle edge case

---
+++ lc88r.java
@@ -0,0 +1,35 @@
+class solution{
+    public void merge(int []nums1,int m,int n,int []nums2)
+    {
+        int i = m - 1;
+        int j = n - 1;
+        int k = m + n - 1;
+
+        while (j >= 0) {
+            if (i >= 0 && nums1[i] > nums2[j]) {
+                nums1[k] = nums1[i];
+                i--;
+            } else {
+                nums1[k] = nums2[j];
+                j--;
+            }
+            k--;
+        }
+
+        for (int l = 0; l < nums1.length; l++) {
+            System.out.print(nums1[l] + ",");
+        }
+    }
+}
+
+class lc88r{
+    public static void main(String args[])
+    {
+        int []nums1={1,2,3,0,0,0};
+        int []nums2={2,5,6};
+        int m=nums1.length-nums2.length;
+        int n=nums2.length;
+        solution s= new solution();
+        s.merge(nums1, m, n, nums2);
+    }
+}
